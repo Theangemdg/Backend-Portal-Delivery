9d003d2ea725d504ed1b2754ab5ed40e0dce40e8
theangemdg <danielsanchez@unah.hn>
2022-04-20 23:11:51 -0600

    api-orden finalizada

---
+++ api/ordenes.php
@@ -0,0 +1,27 @@
+<?php
+
+    include_once("../clases/class-ordenes.php");
+    header("Content-Type: application/json");
+    switch($_SERVER['REQUEST_METHOD']){
+        case 'POST':
+            $_POST = json_decode(file_get_contents('php://input'),true);
+            $orden = new Orden($_POST["nombreProducto"], $_POST["imgProducto"], $_POST["cantidad"], $_POST["descripcion"], $_POST["precio"]);
+            $orden -> guardarOrden($_GET['id']);
+            $resultado["mensaje"] = "Guardar orden, informacion: ".json_encode($_POST);
+            echo json_encode($resultado); 
+        break;
+        case 'GET':
+            
+        break;
+        case 'PUT':
+
+        break;
+        case 'DELETE':
+            Orden::eliminarOrden($_GET["id"], $_GET['idO']);
+            $resultado["mensaje"] = "Eliminar una orden con el id: ".$_GET['id'];
+            echo json_encode($resultado);
+        break;
+    }
+
+?>
+
